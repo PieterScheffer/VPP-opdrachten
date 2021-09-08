c56da9de8176f640aa13844339ecebb5603bfca1
PieterScheffer <89527258+PieterScheffer@users.noreply.github.com>
2021-09-08 12:56:44 +0200

    Add files via upload

---
+++ Opdracht2-VlaggenVanDeWereld/Vlaggen/Form1.Designer.cs
@@ -0,0 +1,304 @@
+﻿
+namespace Vlaggen
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.buttonPanel = new System.Windows.Forms.Panel();
+            this.testButton = new System.Windows.Forms.Button();
+            this.flagPanel = new System.Windows.Forms.Panel();
+            this.buttonPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth16Bit;
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "Afghanistan.png");
+            this.imageList1.Images.SetKeyName(1, "Albanië.png");
+            this.imageList1.Images.SetKeyName(2, "Algerije.png");
+            this.imageList1.Images.SetKeyName(3, "Andorra.png");
+            this.imageList1.Images.SetKeyName(4, "Angola.png");
+            this.imageList1.Images.SetKeyName(5, "Antigua en Barbuda.png");
+            this.imageList1.Images.SetKeyName(6, "Argentinië.png");
+            this.imageList1.Images.SetKeyName(7, "Armenië.png");
+            this.imageList1.Images.SetKeyName(8, "Australië.png");
+            this.imageList1.Images.SetKeyName(9, "Azerbeidzjan.png");
+            this.imageList1.Images.SetKeyName(10, "Bahama\'s.png");
+            this.imageList1.Images.SetKeyName(11, "Bahrein.png");
+            this.imageList1.Images.SetKeyName(12, "Bangladesh.png");
+            this.imageList1.Images.SetKeyName(13, "Barbados.png");
+            this.imageList1.Images.SetKeyName(14, "België.png");
+            this.imageList1.Images.SetKeyName(15, "Belize.png");
+            this.imageList1.Images.SetKeyName(16, "Benin.png");
+            this.imageList1.Images.SetKeyName(17, "Bhutan.png");
+            this.imageList1.Images.SetKeyName(18, "Bolivia.png");
+            this.imageList1.Images.SetKeyName(19, "Bosnië en Herzegovina.png");
+            this.imageList1.Images.SetKeyName(20, "Botswana.png");
+            this.imageList1.Images.SetKeyName(21, "Brazilië.png");
+            this.imageList1.Images.SetKeyName(22, "Brunei.png");
+            this.imageList1.Images.SetKeyName(23, "Bulgarije.png");
+            this.imageList1.Images.SetKeyName(24, "Burkina Faso.png");
+            this.imageList1.Images.SetKeyName(25, "Burundi.png");
+            this.imageList1.Images.SetKeyName(26, "Cambodja.png");
+            this.imageList1.Images.SetKeyName(27, "Canada.png");
+            this.imageList1.Images.SetKeyName(28, "Centraal-Afrikaanse Republiek.png");
+            this.imageList1.Images.SetKeyName(29, "Chili.png");
+            this.imageList1.Images.SetKeyName(30, "Combodia.png");
+            this.imageList1.Images.SetKeyName(31, "Comoren.png");
+            this.imageList1.Images.SetKeyName(32, "Congo-Brazzaville.png");
+            this.imageList1.Images.SetKeyName(33, "Congo-Kinshasa.png");
+            this.imageList1.Images.SetKeyName(34, "Cookeilanden.png");
+            this.imageList1.Images.SetKeyName(35, "Costa Rica.png");
+            this.imageList1.Images.SetKeyName(36, "Cuba.png");
+            this.imageList1.Images.SetKeyName(37, "Cyprus.png");
+            this.imageList1.Images.SetKeyName(38, "Denemarken.png");
+            this.imageList1.Images.SetKeyName(39, "Djibouti.png");
+            this.imageList1.Images.SetKeyName(40, "Dominica.png");
+            this.imageList1.Images.SetKeyName(41, "Dominicaanse Republiek.png");
+            this.imageList1.Images.SetKeyName(42, "Duitsland.png");
+            this.imageList1.Images.SetKeyName(43, "Ecuador.png");
+            this.imageList1.Images.SetKeyName(44, "Egypte.png");
+            this.imageList1.Images.SetKeyName(45, "El Salvador.png");
+            this.imageList1.Images.SetKeyName(46, "Equatoriaaal-Guinea.png");
+            this.imageList1.Images.SetKeyName(47, "Eritrea.png");
+            this.imageList1.Images.SetKeyName(48, "Estland.png");
+            this.imageList1.Images.SetKeyName(49, "Ethiopië.png");
+            this.imageList1.Images.SetKeyName(50, "Fiji.png");
+            this.imageList1.Images.SetKeyName(51, "Filipijnen.png");
+            this.imageList1.Images.SetKeyName(52, "Finland.png");
+            this.imageList1.Images.SetKeyName(53, "Frankrijk.png");
+            this.imageList1.Images.SetKeyName(54, "Gabon.png");
+            this.imageList1.Images.SetKeyName(55, "Gambia.png");
+            this.imageList1.Images.SetKeyName(56, "Georgia.png");
+            this.imageList1.Images.SetKeyName(57, "Ghana.png");
+            this.imageList1.Images.SetKeyName(58, "Grenada.png");
+            this.imageList1.Images.SetKeyName(59, "Griekenland.png");
+            this.imageList1.Images.SetKeyName(60, "Guatemala.png");
+            this.imageList1.Images.SetKeyName(61, "Guinee.png");
+            this.imageList1.Images.SetKeyName(62, "Guinee-Bissau.png");
+            this.imageList1.Images.SetKeyName(63, "Guyna.png");
+            this.imageList1.Images.SetKeyName(64, "Haïti.png");
+            this.imageList1.Images.SetKeyName(65, "Honduras.png");
+            this.imageList1.Images.SetKeyName(66, "Hongarije.png");
+            this.imageList1.Images.SetKeyName(67, "Ierland.png");
+            this.imageList1.Images.SetKeyName(68, "Ijsland.png");
+            this.imageList1.Images.SetKeyName(69, "India.png");
+            this.imageList1.Images.SetKeyName(70, "Indonesië.png");
+            this.imageList1.Images.SetKeyName(71, "Irak.png");
+            this.imageList1.Images.SetKeyName(72, "Iran.png");
+            this.imageList1.Images.SetKeyName(73, "Israel.png");
+            this.imageList1.Images.SetKeyName(74, "Italië.png");
+            this.imageList1.Images.SetKeyName(75, "Ivoorkust.png");
+            this.imageList1.Images.SetKeyName(76, "Jamaica.png");
+            this.imageList1.Images.SetKeyName(77, "Japan.png");
+            this.imageList1.Images.SetKeyName(78, "Jemen.png");
+            this.imageList1.Images.SetKeyName(79, "Jordanië.png");
+            this.imageList1.Images.SetKeyName(80, "Kaapverdië.png");
+            this.imageList1.Images.SetKeyName(81, "Kameroen.png");
+            this.imageList1.Images.SetKeyName(82, "Kazachstan.png");
+            this.imageList1.Images.SetKeyName(83, "Kenia.png");
+            this.imageList1.Images.SetKeyName(84, "Kirgizië.png");
+            this.imageList1.Images.SetKeyName(85, "Kiribati.png");
+            this.imageList1.Images.SetKeyName(86, "Koeweit.png");
+            this.imageList1.Images.SetKeyName(87, "Kosovo.png");
+            this.imageList1.Images.SetKeyName(88, "Kroatië.png");
+            this.imageList1.Images.SetKeyName(89, "Laos.png");
+            this.imageList1.Images.SetKeyName(90, "Lesotho.png");
+            this.imageList1.Images.SetKeyName(91, "Letland.png");
+            this.imageList1.Images.SetKeyName(92, "Libanon.png");
+            this.imageList1.Images.SetKeyName(93, "Liberia.png");
+            this.imageList1.Images.SetKeyName(94, "Libië.png");
+            this.imageList1.Images.SetKeyName(95, "Liechtenstein.png");
+            this.imageList1.Images.SetKeyName(96, "Litouwen.png");
+            this.imageList1.Images.SetKeyName(97, "Luxemburg.png");
+            this.imageList1.Images.SetKeyName(98, "Macedonia.png");
+            this.imageList1.Images.SetKeyName(99, "Madagaskar.png");
+            this.imageList1.Images.SetKeyName(100, "Malawi.png");
+            this.imageList1.Images.SetKeyName(101, "Maldiven.png");
+            this.imageList1.Images.SetKeyName(102, "Maleisië.png");
+            this.imageList1.Images.SetKeyName(103, "Mali.png");
+            this.imageList1.Images.SetKeyName(104, "Malta.png");
+            this.imageList1.Images.SetKeyName(105, "Marokko.png");
+            this.imageList1.Images.SetKeyName(106, "Marshalleilanden.png");
+            this.imageList1.Images.SetKeyName(107, "Mauritanië.png");
+            this.imageList1.Images.SetKeyName(108, "Mauritius.png");
+            this.imageList1.Images.SetKeyName(109, "Mexico.png");
+            this.imageList1.Images.SetKeyName(110, "Micronesië.png");
+            this.imageList1.Images.SetKeyName(111, "Moldavië.png");
+            this.imageList1.Images.SetKeyName(112, "Monaco.png");
+            this.imageList1.Images.SetKeyName(113, "Mongolië.png");
+            this.imageList1.Images.SetKeyName(114, "Montenegro.png");
+            this.imageList1.Images.SetKeyName(115, "Mozambique.png");
+            this.imageList1.Images.SetKeyName(116, "Myanmar.png");
+            this.imageList1.Images.SetKeyName(117, "Nambië.png");
+            this.imageList1.Images.SetKeyName(118, "Nauru.png");
+            this.imageList1.Images.SetKeyName(119, "Nederland.png");
+            this.imageList1.Images.SetKeyName(120, "Nepal.png");
+            this.imageList1.Images.SetKeyName(121, "Nicaragua.png");
+            this.imageList1.Images.SetKeyName(122, "Nieuw-Zeeland.png");
+            this.imageList1.Images.SetKeyName(123, "Niger.png");
+            this.imageList1.Images.SetKeyName(124, "Nigeria.png");
+            this.imageList1.Images.SetKeyName(125, "Niue.png");
+            this.imageList1.Images.SetKeyName(126, "Noord-Korea.png");
+            this.imageList1.Images.SetKeyName(127, "Noorwegen.png");
+            this.imageList1.Images.SetKeyName(128, "Oeganda.png");
+            this.imageList1.Images.SetKeyName(129, "Oekraïne.png");
+            this.imageList1.Images.SetKeyName(130, "Oezbekistan.png");
+            this.imageList1.Images.SetKeyName(131, "Oman.png");
+            this.imageList1.Images.SetKeyName(132, "Oostenrijk.png");
+            this.imageList1.Images.SetKeyName(133, "Oost-Timor.png");
+            this.imageList1.Images.SetKeyName(134, "Pakistan.png");
+            this.imageList1.Images.SetKeyName(135, "Palau.png");
+            this.imageList1.Images.SetKeyName(136, "Palestina.png");
+            this.imageList1.Images.SetKeyName(137, "Panama.png");
+            this.imageList1.Images.SetKeyName(138, "Papoea-Nieuw-Guinea.png");
+            this.imageList1.Images.SetKeyName(139, "Paraguay.png");
+            this.imageList1.Images.SetKeyName(140, "Peru.png");
+            this.imageList1.Images.SetKeyName(141, "Polen.png");
+            this.imageList1.Images.SetKeyName(142, "Portugal.png");
+            this.imageList1.Images.SetKeyName(143, "Qatar.png");
+            this.imageList1.Images.SetKeyName(144, "Roemenië.png");
+            this.imageList1.Images.SetKeyName(145, "Rusland.png");
+            this.imageList1.Images.SetKeyName(146, "Rwanda.png");
+            this.imageList1.Images.SetKeyName(147, "Saint Kitts en Nevis.png");
+            this.imageList1.Images.SetKeyName(148, "Saint Lucia.png");
+            this.imageList1.Images.SetKeyName(149, "Saint Vincent an de Grenadines.png");
+            this.imageList1.Images.SetKeyName(150, "Salomonseilanden.png");
+            this.imageList1.Images.SetKeyName(151, "Sambia.png");
+            this.imageList1.Images.SetKeyName(152, "Samoa.png");
+            this.imageList1.Images.SetKeyName(153, "San Marino.png");
+            this.imageList1.Images.SetKeyName(154, "Sao Tomé en Principe.png");
+            this.imageList1.Images.SetKeyName(155, "Saoedi-Arabië.png");
+            this.imageList1.Images.SetKeyName(156, "Senegal.png");
+            this.imageList1.Images.SetKeyName(157, "Servië.png");
+            this.imageList1.Images.SetKeyName(158, "Seychellen.png");
+            this.imageList1.Images.SetKeyName(159, "Sierra Leone.png");
+            this.imageList1.Images.SetKeyName(160, "Singapore.png");
+            this.imageList1.Images.SetKeyName(161, "Slovenië.png");
+            this.imageList1.Images.SetKeyName(162, "Slowakije.png");
+            this.imageList1.Images.SetKeyName(163, "Soedan.png");
+            this.imageList1.Images.SetKeyName(164, "Somalië.png");
+            this.imageList1.Images.SetKeyName(165, "Spanje.png");
+            this.imageList1.Images.SetKeyName(166, "Sri Lanka.png");
+            this.imageList1.Images.SetKeyName(167, "surimane.png");
+            this.imageList1.Images.SetKeyName(168, "Swaziland.png");
+            this.imageList1.Images.SetKeyName(169, "Syrië.png");
+            this.imageList1.Images.SetKeyName(170, "Tadzijkistan.png");
+            this.imageList1.Images.SetKeyName(171, "Taiwan.png");
+            this.imageList1.Images.SetKeyName(172, "Tanzanoa.png");
+            this.imageList1.Images.SetKeyName(173, "Thailand.png");
+            this.imageList1.Images.SetKeyName(174, "Togo.png");
+            this.imageList1.Images.SetKeyName(175, "Tonga.png");
+            this.imageList1.Images.SetKeyName(176, "Trinidad en Tobago.png");
+            this.imageList1.Images.SetKeyName(177, "Tsjaad.png");
+            this.imageList1.Images.SetKeyName(178, "Tsjechië.png");
+            this.imageList1.Images.SetKeyName(179, "Tunesië.png");
+            this.imageList1.Images.SetKeyName(180, "Turkije.png");
+            this.imageList1.Images.SetKeyName(181, "Turkmenistan.png");
+            this.imageList1.Images.SetKeyName(182, "Tuvalu.png");
+            this.imageList1.Images.SetKeyName(183, "Uruguay.png");
+            this.imageList1.Images.SetKeyName(184, "Vanuatu.png");
+            this.imageList1.Images.SetKeyName(185, "Vaticaanstad.png");
+            this.imageList1.Images.SetKeyName(186, "Venezuela.png");
+            this.imageList1.Images.SetKeyName(187, "Verebigde Staten.png");
+            this.imageList1.Images.SetKeyName(188, "Verenigd Koningkrijk.png");
+            this.imageList1.Images.SetKeyName(189, "Verenigde Arabische Emiraten.png");
+            this.imageList1.Images.SetKeyName(190, "Vietnam.png");
+            this.imageList1.Images.SetKeyName(191, "Volksrepubliek China.png");
+            this.imageList1.Images.SetKeyName(192, "Westerlijke Sahara.png");
+            this.imageList1.Images.SetKeyName(193, "Wit Rusland.png");
+            this.imageList1.Images.SetKeyName(194, "Zimbabwe.png");
+            this.imageList1.Images.SetKeyName(195, "Zuid-Afrika.png");
+            this.imageList1.Images.SetKeyName(196, "Zuid-Korea.png");
+            this.imageList1.Images.SetKeyName(197, "Zuid-soedan.png");
+            this.imageList1.Images.SetKeyName(198, "Zweden.png");
+            this.imageList1.Images.SetKeyName(199, "Zwitserland.png");
+            // 
+            // buttonPanel
+            // 
+            this.buttonPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.buttonPanel.Controls.Add(this.testButton);
+            this.buttonPanel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.buttonPanel.Location = new System.Drawing.Point(0, 469);
+            this.buttonPanel.Name = "buttonPanel";
+            this.buttonPanel.Size = new System.Drawing.Size(835, 114);
+            this.buttonPanel.TabIndex = 0;
+            // 
+            // testButton
+            // 
+            this.testButton.Location = new System.Drawing.Point(364, 25);
+            this.testButton.Name = "testButton";
+            this.testButton.Size = new System.Drawing.Size(103, 45);
+            this.testButton.TabIndex = 1;
+            this.testButton.Text = "Test";
+            this.testButton.UseVisualStyleBackColor = true;
+            this.testButton.Click += new System.EventHandler(this.test_CLick);
+            // 
+            // flagPanel
+            // 
+            this.flagPanel.AutoScroll = true;
+            this.flagPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.flagPanel.Location = new System.Drawing.Point(0, 0);
+            this.flagPanel.Name = "flagPanel";
+            this.flagPanel.Size = new System.Drawing.Size(835, 463);
+            this.flagPanel.TabIndex = 0;
+            this.flagPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.ClientSize = new System.Drawing.Size(835, 583);
+            this.Controls.Add(this.flagPanel);
+            this.Controls.Add(this.buttonPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(816, 500);
+            this.Name = "Form1";
+            this.Text = "Vlaggen van de wereld";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.buttonPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Panel buttonPanel;
+        private System.Windows.Forms.Panel flagPanel;
+        private System.Windows.Forms.Button testButton;
+    }
+}
+
---
+++ Opdracht2-VlaggenVanDeWereld/Vlaggen/Form1.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Vlaggen
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+            int hLoc = 60;
+            int vLoc = 50;
+
+            
+             
+            foreach (string flags in imageList1.Images.Keys) 
+            {
+                int screenWidth = flagPanel.Width;
+                int screenHeight = flagPanel.Height;
+
+
+                PictureBox flag = new PictureBox();
+                flag.Image = imageList1.Images[flags];
+
+                flag.Height = 110;
+                flag.Width = 165;
+                flag.Name = flags;
+                flag.Location = new Point(hLoc, vLoc);
+                flagPanel.Controls.Add(flag);
+
+                int flagNameVLoc = vLoc + 110;
+                int flagNameHLoc = hLoc + 30;
+
+                Label flagName = new Label();
+                flagName.Text = flag.Name;
+                int i = flagName.Text.IndexOf('.');
+                flagName.Text = flagName.Text.Substring(0, i);
+                flagName.BackColor = Color.FromArgb(20, 20, 20);
+                flagName.ForeColor = Color.White;
+                flagName.BorderStyle = BorderStyle.None;
+                flagName.TextAlign = ContentAlignment.MiddleCenter;
+                flagName.Location = new Point(flagNameHLoc, flagNameVLoc);
+                flagPanel.Controls.Add(flagName);
+
+                flag.Height = 110;
+                flag.Width = 165;
+                flag.Name = flags;
+                flag.Location = new Point(hLoc, vLoc);
+                flagPanel.Controls.Add(flag);
+
+                hLoc += 175;
+
+                if (flag.Location.X + flag.Width >= screenWidth - flag.Width)
+                {
+                    hLoc = 60;
+                    vLoc += 150;
+                }
+
+                
+
+            }
+
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+            
+
+        }
+
+        Panel testPanel = new Panel();
+        PictureBox testFlag = new PictureBox();
+        Panel answerPanel = new Panel();
+        Label correctCount = new Label();
+        int counter = 0;
+        string answer;
+        public void test_CLick(object sender, EventArgs e)
+        {
+            flagPanel.Hide();
+            buttonPanel.Hide();
+
+
+            
+            testPanel.Dock = DockStyle.Top;
+            testPanel.Height = 440;
+            Controls.Add(testPanel);
+
+            answerPanel.Dock = DockStyle.Bottom;
+            answerPanel.Height = 140;
+            answerPanel.BackColor = Color.FromArgb(30, 30, 30);
+            Controls.Add(answerPanel);
+
+            correctCount.Dock = DockStyle.Right;
+            correctCount.BackColor = Color.FromArgb(30, 30, 30);
+            correctCount.ForeColor = Color.White;
+            correctCount.BorderStyle = BorderStyle.None;
+            correctCount.Text = string.Format("Correct: {0}", counter);
+            answerPanel.Controls.Add(correctCount);
+
+            Random rng = new Random();
+
+            
+
+            testFlag.Size = new Size(200, 200);
+            int x = 340;
+            int y = 200;
+            testFlag.Location = new Point(x, y);
+
+
+
+
+            int rngFlag = rng.Next(imageList1.Images.Count);
+            testFlag.Image = imageList1.Images[rngFlag];
+            testPanel.Controls.Add(testFlag);
+            testFlag.Name = imageList1.Images.Keys[rngFlag].ToString();
+            int i = testFlag.Name.IndexOf('.');
+            string option1 = testFlag.Name.Substring(0, i);
+
+            rngFlag = rng.Next(imageList1.Images.Count);
+            testFlag.Image = imageList1.Images[rngFlag];
+            testPanel.Controls.Add(testFlag);
+            testFlag.Name = imageList1.Images.Keys[rngFlag].ToString();
+            i = testFlag.Name.IndexOf('.');
+            string option2 = testFlag.Name.Substring(0, i);
+
+            rngFlag = rng.Next(imageList1.Images.Count);
+            testFlag.Image = imageList1.Images[rngFlag];
+            testPanel.Controls.Add(testFlag);
+            testFlag.Name = imageList1.Images.Keys[rngFlag].ToString();
+            i = testFlag.Name.IndexOf('.');
+            string option3 = testFlag.Name.Substring(0, i);
+
+            rngFlag = rng.Next(imageList1.Images.Count);
+            testFlag.Image = imageList1.Images[rngFlag];
+            testPanel.Controls.Add(testFlag);
+            testFlag.Name = imageList1.Images.Keys[rngFlag].ToString();
+            i = testFlag.Name.IndexOf('.');
+            answer = testFlag.Name.Substring(0, i);
+
+
+            string[] options = new string[4];
+            options[0] = answer;
+            options[1] = option1;
+            options[2] = option2;
+            options[3] = option3;
+
+            var indexes = Enumerable.Range(0, 4).OrderBy(i => rng.Next());
+
+            int ansX = 265;
+            int ansY = 5;
+            
+
+            foreach (int index in indexes)
+            {
+                Button answerButton = new Button();
+                answerButton.Width = 300;
+                answerButton.Height = 25;
+                answerButton.FlatStyle = FlatStyle.Flat;
+                answerButton.BackColor = Color.White;
+                answerButton.Location = new Point(ansX, ansY);
+                string answerText = options[index];
+                answerButton.Text = answerText;
+                answerPanel.Controls.Add(answerButton);
+                answerButton.Click += answer_CLick;
+
+                ansY += 30;
+
+                
+            }
+
+
+            
+            Button backButton = new Button();
+            backButton.BackColor = Color.White;
+            backButton.Text = "Back";
+            answerPanel.Controls.Add(backButton);
+            backButton.Click += back_CLick;
+            
+            
+        }
+        
+        private void answer_CLick(object sender, EventArgs e)
+        {
+                Button button = (Button)sender;
+
+                
+
+                if (button.Text == answer)
+                {
+                
+                    correctCount.Text = string.Format("Correct: {0}", counter);
+                    counter += 1;
+                }
+                answerPanel.Controls.Clear();
+                test_CLick(this, new EventArgs());
+
+        }
+
+        private void back_CLick(object sender, EventArgs e)
+        {
+            counter = 0;
+
+            flagPanel.Show();
+            buttonPanel.Show();
+
+            Controls.Remove(testPanel);
+            testFlag.Controls.Clear();
+            Controls.Remove(answerPanel);
+            answerPanel.Controls.Clear();
+
+
+
+        }
+
+    }
+
+}
