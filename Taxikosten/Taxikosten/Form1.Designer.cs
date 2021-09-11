1f2c81d6ba7fb2c1fdd96b5639e94b98676e319d
Pieter Scheffer <pieterscheffer@outlook.com>
2021-09-11 17:58:01 +0200

    New project

---
+++ Taxikosten/Taxikosten/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿
+namespace Taxikosten
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.resultTextBox = new System.Windows.Forms.TextBox();
+            this.calc_Button = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.startDateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.endDateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // resultTextBox
+            // 
+            this.resultTextBox.Location = new System.Drawing.Point(166, 116);
+            this.resultTextBox.Name = "resultTextBox";
+            this.resultTextBox.Size = new System.Drawing.Size(81, 20);
+            this.resultTextBox.TabIndex = 10;
+            // 
+            // calc_Button
+            // 
+            this.calc_Button.Location = new System.Drawing.Point(47, 113);
+            this.calc_Button.Name = "calc_Button";
+            this.calc_Button.Size = new System.Drawing.Size(67, 23);
+            this.calc_Button.TabIndex = 11;
+            this.calc_Button.Text = "Resultaat";
+            this.calc_Button.UseVisualStyleBackColor = true;
+            this.calc_Button.Click += new System.EventHandler(this.calc_Button_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(128, 119);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(13, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "=";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(147, 119);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(13, 13);
+            this.label6.TabIndex = 19;
+            this.label6.Text = "€";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // startDateTimePicker
+            // 
+            this.startDateTimePicker.Location = new System.Drawing.Point(47, 33);
+            this.startDateTimePicker.Name = "startDateTimePicker";
+            this.startDateTimePicker.Size = new System.Drawing.Size(200, 20);
+            this.startDateTimePicker.TabIndex = 20;
+            // 
+            // endDateTimePicker
+            // 
+            this.endDateTimePicker.Location = new System.Drawing.Point(47, 76);
+            this.endDateTimePicker.Name = "endDateTimePicker";
+            this.endDateTimePicker.Size = new System.Drawing.Size(200, 20);
+            this.endDateTimePicker.TabIndex = 21;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 33);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.TabIndex = 22;
+            this.label3.Text = "Start";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 13);
+            this.label1.TabIndex = 23;
+            this.label1.Text = "Eind";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(280, 158);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.endDateTimePicker);
+            this.Controls.Add(this.startDateTimePicker);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.calc_Button);
+            this.Controls.Add(this.resultTextBox);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "TaxiKosten";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox resultTextBox;
+        private System.Windows.Forms.Button calc_Button;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DateTimePicker startDateTimePicker;
+        private System.Windows.Forms.DateTimePicker endDateTimePicker;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Taxikosten/Taxikosten/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Taxikosten
+{
+    public partial class Form1 : Form
+    {
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            startDateTimePicker.Format = DateTimePickerFormat.Custom;
+            startDateTimePicker.CustomFormat = "dddd d-M-yyyy   HH : mm";
+
+            endDateTimePicker.Format = DateTimePickerFormat.Custom;
+            endDateTimePicker.CustomFormat = "dddd d-M-yyyy   HH : mm";
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void calc_Button_Click(object sender, EventArgs e)
+        {
+      
+            decimal minutes = Convert.ToDecimal(endDateTimePicker.Value.Subtract(startDateTimePicker.Value).TotalMinutes);
+
+            if (startDateTimePicker.Value.DayOfWeek == DayOfWeek.Friday && startDateTimePicker.Value.Hour > 22 || startDateTimePicker.Value.DayOfWeek == DayOfWeek.Saturday || startDateTimePicker.Value.DayOfWeek == DayOfWeek.Sunday
+               || startDateTimePicker.Value.DayOfWeek == DayOfWeek.Monday && startDateTimePicker.Value.Hour < 7)
+            {
+                if (startDateTimePicker.Value.Hour < 8 && startDateTimePicker.Value.Hour > 18)
+                    resultTextBox.Text = Math.Round((minutes * 0.45m) * 1.15m, 2).ToString();
+                else
+                    resultTextBox.Text = Math.Round((minutes * 0.25m) * 1.15m, 2).ToString();
+
+            }
+            else
+            {
+
+                if (startDateTimePicker.Value.Hour < 8 && startDateTimePicker.Value.Hour > 18)
+                    resultTextBox.Text = Math.Round(minutes * 0.45m, 2).ToString();
+                else
+                    resultTextBox.Text = Math.Round(minutes * 0.25m, 2).ToString();
+
+            }
+            
+
+
+
+        }
+
+    }
+}
