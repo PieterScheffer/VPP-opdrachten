a5015f1403459d964dcea9e4f4dec20069217f16
PieterScheffer <89527258+PieterScheffer@users.noreply.github.com>
2021-09-12 18:51:24 +0200

    Add files via upload

---
+++ AutoVerhuur/AutoVerhuur/Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿
+namespace AutoVerhuur
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startDateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.endDateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.kmUpDown = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.litersUpDown = new System.Windows.Forms.NumericUpDown();
+            this.label5 = new System.Windows.Forms.Label();
+            this.resultTextBox = new System.Windows.Forms.TextBox();
+            this.resultButton = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.autoRadioButton = new System.Windows.Forms.RadioButton();
+            this.busRadioButton = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.kmUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.litersUpDown)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // startDateTimePicker
+            // 
+            this.startDateTimePicker.Location = new System.Drawing.Point(87, 49);
+            this.startDateTimePicker.Name = "startDateTimePicker";
+            this.startDateTimePicker.Size = new System.Drawing.Size(200, 20);
+            this.startDateTimePicker.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Startdatum";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Einddatum";
+            // 
+            // endDateTimePicker
+            // 
+            this.endDateTimePicker.Location = new System.Drawing.Point(87, 89);
+            this.endDateTimePicker.Name = "endDateTimePicker";
+            this.endDateTimePicker.Size = new System.Drawing.Size(200, 20);
+            this.endDateTimePicker.TabIndex = 2;
+            // 
+            // kmUpDown
+            // 
+            this.kmUpDown.Location = new System.Drawing.Point(87, 128);
+            this.kmUpDown.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.kmUpDown.Name = "kmUpDown";
+            this.kmUpDown.Size = new System.Drawing.Size(99, 20);
+            this.kmUpDown.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 128);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Kilometers";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 165);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Liters benzine";
+            // 
+            // litersUpDown
+            // 
+            this.litersUpDown.Location = new System.Drawing.Point(87, 165);
+            this.litersUpDown.Name = "litersUpDown";
+            this.litersUpDown.Size = new System.Drawing.Size(99, 20);
+            this.litersUpDown.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 202);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(40, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Kosten";
+            // 
+            // resultTextBox
+            // 
+            this.resultTextBox.Location = new System.Drawing.Point(187, 202);
+            this.resultTextBox.Name = "resultTextBox";
+            this.resultTextBox.Size = new System.Drawing.Size(100, 20);
+            this.resultTextBox.TabIndex = 9;
+            // 
+            // resultButton
+            // 
+            this.resultButton.Location = new System.Drawing.Point(87, 202);
+            this.resultButton.Name = "resultButton";
+            this.resultButton.Size = new System.Drawing.Size(75, 23);
+            this.resultButton.TabIndex = 10;
+            this.resultButton.Text = "Resultaat";
+            this.resultButton.UseVisualStyleBackColor = true;
+            this.resultButton.Click += new System.EventHandler(this.resultButton_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 19);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(46, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Voertuig";
+            // 
+            // autoRadioButton
+            // 
+            this.autoRadioButton.AutoSize = true;
+            this.autoRadioButton.Location = new System.Drawing.Point(6, 9);
+            this.autoRadioButton.Name = "autoRadioButton";
+            this.autoRadioButton.Size = new System.Drawing.Size(47, 17);
+            this.autoRadioButton.TabIndex = 12;
+            this.autoRadioButton.TabStop = true;
+            this.autoRadioButton.Text = "Auto";
+            this.autoRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // busRadioButton
+            // 
+            this.busRadioButton.AutoSize = true;
+            this.busRadioButton.Location = new System.Drawing.Point(59, 9);
+            this.busRadioButton.Name = "busRadioButton";
+            this.busRadioButton.Size = new System.Drawing.Size(43, 17);
+            this.busRadioButton.TabIndex = 13;
+            this.busRadioButton.TabStop = true;
+            this.busRadioButton.Text = "Bus";
+            this.busRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.busRadioButton);
+            this.groupBox1.Controls.Add(this.autoRadioButton);
+            this.groupBox1.Location = new System.Drawing.Point(87, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(110, 31);
+            this.groupBox1.TabIndex = 14;
+            this.groupBox1.TabStop = false;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(168, 205);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(13, 13);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "€";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(303, 249);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.resultButton);
+            this.Controls.Add(this.resultTextBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.litersUpDown);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.kmUpDown);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.endDateTimePicker);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.startDateTimePicker);
+            this.Name = "Form1";
+            this.Text = "Autoverhuur";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.kmUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.litersUpDown)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker startDateTimePicker;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker endDateTimePicker;
+        private System.Windows.Forms.NumericUpDown kmUpDown;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown litersUpDown;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox resultTextBox;
+        private System.Windows.Forms.Button resultButton;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.RadioButton autoRadioButton;
+        private System.Windows.Forms.RadioButton busRadioButton;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ AutoVerhuur/AutoVerhuur/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AutoVerhuur
+{
+    public partial class Form1 : Form
+    {
+        
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            //Sets button to checket to make sure it isnt empty.
+            autoRadioButton.Checked = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void resultButton_Click(object sender, EventArgs e)
+        {
+            decimal kmPrice;
+            decimal dayRate;
+            decimal kmPriceTotal;
+
+            decimal gasolinePrice = 1.793m;
+            //Gets number of days between start and enddate, adds one to include today and converts to decimal.
+            decimal days = Convert.ToDecimal(endDateTimePicker.Value.Subtract(startDateTimePicker.Value).TotalDays + 1);
+            //Tests wich radiobutton is chacked and assigns corresponding value.
+            if (autoRadioButton.Checked == true)
+            {
+                kmPrice = 0.20m;
+                dayRate = 50;
+            }
+            else
+            {
+                kmPrice = 0.30m;
+                dayRate = 95;
+            }
+            //Tests if input is below 100 and stores it as 0 or input - 100.
+            if (kmUpDown.Value < 100)
+            {
+                kmPriceTotal = 0;
+            }
+            else
+            {
+                kmPriceTotal = (kmUpDown.Value - 100) * kmPrice;
+            }
+            //Calculates total and puts it in resultTextbox.Text.
+            resultTextBox.Text = Math.Round((days * dayRate) + kmPriceTotal + (litersUpDown.Value * gasolinePrice),2).ToString();
+
+        }
+    }
+}
